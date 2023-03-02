dd7308da320a401e4d96076539f67863e04c1092
megalocoman <cfjofre@uc.cl>
2023-03-02 16:05:46 -0300

    main doing recursive search done

--- crawl.js
+++ crawl.js
@@ -1,6 +1,22 @@
 const {JSDOM} = require('jsdom');
 
-async function  crawlPage(currentURL){
+async function  crawlPage(baseURL, currentURL, pages){
+    console.log(`actively crawling: ${currentURL}`);
+
+    const baseURLObj = new URL (baseURL);
+    const currentURLObj = new URL (currentURL);
+
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages;
+    }
+
+    const normalizedCurentUrl = normalizeUrl(currentURL);
+    if(pages[normalizedCurentUrl] >0){
+        pages[normalizedCurentUrl]++;
+        return pages;
+    }
+
+    pages[normalizedCurentUrl] = 1;
     console.log(`actively crawling: ${currentURL}`);
 
     try {
@@ -8,20 +24,29 @@ async function  crawlPage(currentURL){
 
         if(resp.status >399){
             console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`);
-            return;
+            return pages;
         }
 
         const contentType = resp.headers.get("content-type");
         if(!contentType.includes("text/html")  ){
             console.log(`non html response, content type: ${contentType} on page ${currentURL}`);
-            return;
+            return pages;
         }
 
-        console.log(await resp.text());
+        const htmlBody =await resp.text() ;
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages);
+
+        }
+
     } catch (err) {
-        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`);
     }
 
+    return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
